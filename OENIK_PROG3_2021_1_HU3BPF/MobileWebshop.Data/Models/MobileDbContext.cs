7455c8615fc575babaeadf30d33de090741be950
DobiaszMartin <dobiaszmartin@gmail.com>
2021-03-07 20:55:46 +0100

    Db contex created

---
+++ OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Data/Models/MobileDbContext.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="MobileDbContext.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+namespace MobileWebshop.Data.Models
+{
+    using Microsoft.EntityFrameworkCore;
+
+    /// <summary>
+    /// Mobile databes creater.
+    /// </summary>
+    public class MobileDbContext : DbContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MobileDbContext"/> class.
+        /// This constructor ceart a Databes.
+        /// </summary>
+        public MobileDbContext()
+        {
+        }
+
+        /// <summary>
+        /// Gets or Sets Manufactures.
+        /// </summary>
+        public virtual DbSet<Manufacturer> Manufacturers { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Brands.
+        /// </summary>
+        public virtual DbSet<Brand> Brands { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Products.
+        /// </summary>
+        public virtual DbSet<Product> Products { get; set; }
+    }
+}
