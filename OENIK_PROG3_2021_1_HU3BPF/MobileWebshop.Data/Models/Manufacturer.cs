52adc2d50c91d3932618284af4b966e99db9cb6b
DobiaszMartin <dobiaszmartin@gmail.com>
2021-03-09 10:21:17 +0100

    fixed Manufacturer class name

--- OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Data/Models/Manufacturer.cs
+++ OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Data/Models/Manufacturer.cs
@@ -1,12 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="Manufacturer.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace MobileWebshop.Data.Models
 {
-    class Manufacturer
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    /// <summary>
+    /// Manufacturer table.
+    /// </summary>
+    [Table("Manufacturer")]
+    public class Manufacturer
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Manufacturer"/> class.
+        /// </summary>
+        public Manufacturer()
+        {
+            this.Brands = new HashSet<Brand>();
+        }
+
+        /// <summary>
+        /// Gets or sets manufacturer id.
+        /// </summary>
+        [Key]
+        public int ManufacturerId { get; set; }
+
+        /// <summary>
+        /// gets or sets manufacturer name.
+        /// </summary>
+        [MaxLength(100)]
+        public string ManufacturerName { get; set; }
+
+        /// <summary>
+        /// gets or sets manufacturer Ceo.
+        /// </summary>
+        public string ManufacturerCEO { get; set; }
+
+        /// <summary>
+        /// gets or sets manufacturer center place.
+        /// </summary>
+        public string ManufacturerCenter { get; set; }
+
+        /// <summary>
+        /// gets or sets manufacturer reliability.
+        /// </summary>
+        [Range(0, 10)]
+        public int ManufacturerReliability { get; set; }
+
+        /// <summary>
+        /// gets or sets manufacturer workers members count.
+        /// </summary>
+        public int ManufacturerWorkersCount { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Brand Collection.
+        /// </summary>
+        [NotMapped]
+        public virtual ICollection<Brand> Brands { get; set; }
     }
 }
