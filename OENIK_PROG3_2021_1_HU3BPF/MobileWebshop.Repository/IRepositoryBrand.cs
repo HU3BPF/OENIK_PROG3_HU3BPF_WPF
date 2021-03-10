b83f963f0a15f70d784fc5f40cf3f22d2004d2dc
DobiaszMartin <dobiaszmartin@gmail.com>
2021-03-10 21:48:07 +0100

    Brand IRepository update and Yearupdater method added

--- OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Repository/IRepositoryBrand.cs
+++ OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Repository/IRepositoryBrand.cs
@@ -12,8 +12,24 @@ namespace MobileWebshop.Repository
     public interface IRepositoryBrand : IRepository<Brand>
     {
         /// <summary>
-        /// Brand Id Changer.
+        /// Brand Quality changer.
         /// </summary>
-        void BrandIdChanger();
+        /// <param name="id">Brand Id.</param>
+        /// <param name="newQuality">New brand quality.</param>
+        void BrandQualityChanger(int id, int newQuality);
+
+        /// <summary>
+        /// Brand Update.
+        /// </summary>
+        /// <param name="id">Old Brand id.</param>
+        /// <param name="brand">New brand.</param>
+        void BrandUpdate(int id, Brand brand);
+
+        /// <summary>
+        /// Brand year fixer.
+        /// </summary>
+        /// <param name="id">Brand id.</param>
+        /// <param name="year">FIxed year.</param>
+        void BrandYearFixer(int id, int year);
     }
 }
