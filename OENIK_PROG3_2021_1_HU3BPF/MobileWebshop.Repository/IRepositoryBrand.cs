2845515ab101f1bc23810c7fc60fd383a6a1a552
DobiaszMartin <dobiaszmartin@gmail.com>
2021-03-10 08:57:43 +0100

    Brand Repository created

---
+++ OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Repository/IRepositoryBrand.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="IRepositoryBrand.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace MobileWebshop.Repository
+{
+    using MobileWebshop.Data.Models;
+
+    /// <summary>
+    /// Brand Repository.
+    /// </summary>
+    public interface IRepositoryBrand : IRepository<Brand>
+    {
+        /// <summary>
+        /// Brand Id Changer.
+        /// </summary>
+        void BrandIdChanger();
+    }
+}
