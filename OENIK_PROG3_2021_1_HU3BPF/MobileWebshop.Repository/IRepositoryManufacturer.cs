0818ebe424d740b74717788be7a71363b7ab6bea
DobiaszMartin <dobiaszmartin@gmail.com>
2021-03-10 08:57:55 +0100

    Manufacturer repository created

---
+++ OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Repository/IRepositoryManufacturer.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="IRepositoryManufacturer.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace MobileWebshop.Repository
+{
+    using MobileWebshop.Data.Models;
+
+    /// <summary>
+    /// Manufacturer Repository.
+    /// </summary>
+    public interface IRepositoryManufacturer : IRepository<Manufacturer>
+    {
+        /// <summary>
+        /// Manufacturer Id Changer.
+        /// </summary>
+        void ManufacturerIdChanger();
+    }
+}
