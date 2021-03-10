04d638b8bceb471436477ed85eec20d224f82274
DobiaszMartin <dobiaszmartin@gmail.com>
2021-03-10 21:49:20 +0100

    Category,Price,Color changer method added.

--- OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Repository/IRepositoryProduct.cs
+++ OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Repository/IRepositoryProduct.cs
@@ -15,5 +15,26 @@ namespace MobileWebshop.Repository
         /// Product Id Changer.
         /// </summary>
         void ProductIdChanger();
+
+        /// <summary>
+        /// Product price changer.
+        /// </summary>
+        /// <param name="id">Product id.</param>
+        /// <param name="price">Product new Price.</param>
+        void ProductPriceChanger(int id, int price);
+
+        /// <summary>
+        /// Product categori changer.
+        /// </summary>
+        /// <param name="id">Product id.</param>
+        /// <param name="category">new category.</param>
+        void ProductCategoryChanger(int id, Category category);
+
+        /// <summary>
+        /// Product colour changer.
+        /// </summary>
+        /// <param name="id">Product id.</param>
+        /// <param name="colour">Product new Price.</param>
+        void ProductColorChanger(int id, string colour);
     }
 }
