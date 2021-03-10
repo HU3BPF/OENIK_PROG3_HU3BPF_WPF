380ddf248322deae41e99fbfd413188abcf0d65c
DobiaszMartin <dobiaszmartin@gmail.com>
2021-03-10 22:34:25 +0100

    Manufacturer repository

--- OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Repository/IRepositoryManufacturer.cs
+++ OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Repository/IRepositoryManufacturer.cs
@@ -15,5 +15,33 @@ namespace MobileWebshop.Repository
         /// Manufacturer Id Changer.
         /// </summary>
         void ManufacturerIdChanger();
+
+        /// <summary>
+        /// Manufacturer CEO update.
+        /// </summary>
+        /// <param name="id">Manufacturer id.</param>
+        /// <param name="ceo">New CEO.</param>
+        void ManufacturerCEOChanger(int id, string ceo);
+
+        /// <summary>
+        /// Manufacturer uptare.
+        /// </summary>
+        /// <param name="id">Manufacturer id.</param>
+        /// <param name="manufacturer">New manufacturer.</param>
+        void ManufacturerUpdate(int id, Manufacturer manufacturer);
+
+        /// <summary>
+        /// Manufacturer reliability.
+        /// </summary>
+        /// <param name="id">Manufacturer id.</param>
+        /// <param name="reliability">New manufacturer reliability.</param>
+        void ManufacturerReliabilityChanger(int id, int reliability);
+
+        /// <summary>
+        /// Workers number changer.
+        /// </summary>
+        /// <param name="id">Manufacturer id.</param>
+        /// <param name="workers">New workers number.</param>
+        void ManufacturerWorkersCountChanger(int id, int workers);
     }
 }
