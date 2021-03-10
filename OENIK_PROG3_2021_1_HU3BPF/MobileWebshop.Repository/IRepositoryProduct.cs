d30f856a155c17c95f8049c05111c37fdcc7107f
DobiaszMartin <dobiaszmartin@gmail.com>
2021-03-10 08:58:06 +0100

    Product repository created

---
+++ OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Repository/IRepositoryProduct.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="IRepositoryProduct.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace MobileWebshop.Repository
+{
+    using MobileWebshop.Data.Models;
+
+    /// <summary>
+    /// Product Repository.
+    /// </summary>
+    public interface IRepositoryProduct : IRepository<Product>
+    {
+        /// <summary>
+        /// Product Id Changer.
+        /// </summary>
+        void ProductIdChanger();
+    }
+}
