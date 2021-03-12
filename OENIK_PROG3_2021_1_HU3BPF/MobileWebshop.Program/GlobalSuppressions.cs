c81f7cae14ef8378439efe36daf91c6dfd924935
DobiaszMartin <dobiaszmartin@gmail.com>
2021-03-12 09:56:57 +0100

    suppression 8034

--- OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Program/GlobalSuppressions.cs
+++ OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Program/GlobalSuppressions.cs
@@ -9,3 +9,4 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("NikGitStats", "CA1014", Justification = "<NikGitStats>", Scope = "module")]
+[assembly: SuppressMessage("NikGitStats", "CS8034", Justification = "<NikGitStats>", Scope = "module")]
