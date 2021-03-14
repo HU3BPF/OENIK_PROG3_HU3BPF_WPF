75883bd297a3065268fc93c6e8ff5c10ebba4df2
DobiaszMartin <dobiaszmartin@gmail.com>
2021-03-14 16:26:30 +0100

    RepositoryProduct class added

--- OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Repository/IRepositoryProduct.cs
+++ OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Repository/IRepositoryProduct.cs
@@ -4,6 +4,9 @@
 
 namespace MobileWebshop.Repository
 {
+    using System;
+    using System.Linq;
+    using Microsoft.EntityFrameworkCore;
     using MobileWebshop.Data.Models;
 
     /// <summary>
@@ -11,10 +14,12 @@ namespace MobileWebshop.Repository
     /// </summary>
     public interface IRepositoryProduct : IRepository<Product>
     {
-        /// <summary>
-        /// Product Id Changer.
-        /// </summary>
-        void ProductIdChanger();
+       /// <summary>
+       /// Product ID changer.
+       /// </summary>
+       /// <param name="id">old id.</param>
+       /// <param name="newID">new id.</param>
+        void ProductIdChanger(int id, int newID);
 
         /// <summary>
         /// Product price changer.
@@ -36,5 +41,98 @@ namespace MobileWebshop.Repository
         /// <param name="id">Product id.</param>
         /// <param name="colour">Product new Price.</param>
         void ProductColorChanger(int id, string colour);
+    }
+
+    /// <summary>
+    /// Repository class of Product.
+    /// </summary>
+    public class RepositoryProduct : Repository<Product>, IRepositoryProduct
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RepositoryProduct"/> class.
+        /// </summary>
+        /// <param name="ctx">DBcontext.</param>
+        public RepositoryProduct(DbContext ctx)
+            : base(ctx)
+        {
+        }
+
+        /// <summary>
+        /// One Entity reader.
+        /// </summary>
+        /// <param name="id">Entity id.</param>
+        /// <returns>Entity value.</returns>
+        public override Product GetOne(int id)
+        {
+            return this.GetALL().SingleOrDefault(x => x.ProductdId == id);
+        }
+
+        /// <summary>
+        /// Product categori changer.
+        /// </summary>
+        /// <param name="id">Product id.</param>
+        /// <param name="category">new category.</param>
+        public void ProductCategoryChanger(int id, Category category)
+        {
+            var oldProduct = this.GetOne(id);
+            if (oldProduct == null)
+            {
+                throw new InvalidOperationException("Not found");
+            }
+
+            oldProduct.ProductCategori = category;
+            this.Ctx.SaveChanges();
+        }
+
+        /// <summary>
+        /// Product colour changer.
+        /// </summary>
+        /// <param name="id">Product id.</param>
+        /// <param name="colour">Product new Price.</param>
+        public void ProductColorChanger(int id, string colour)
+        {
+            var oldProduct = this.GetOne(id);
+            if (oldProduct == null)
+            {
+                throw new InvalidOperationException("Not found");
+            }
+
+            oldProduct.ProductColour = colour;
+            this.Ctx.SaveChanges();
+        }
+
+        /// <summary>
+        /// Product ID changer.
+        /// </summary>
+        /// <param name="id">old id.</param>
+        /// <param name="newID">new id.</param>
+        public void ProductIdChanger(int id, int newID)
+        {
+            var oldProduct = this.GetOne(id);
+            if (oldProduct == null)
+            {
+                throw new InvalidOperationException("Not found");
+            }
+
+            oldProduct.ProductdId = newID;
+            this.Ctx.SaveChanges();
+        }
+
+        /// <summary>
+        /// Product price changer.
+        /// </summary>
+        /// <param name="id">Product id.</param>
+        /// <param name="price">Product new Price.</param>
+        public void ProductPriceChanger(int id, int price)
+        {
+            var oldProduct = this.GetOne(id);
+            if (oldProduct == null)
+            {
+                throw new InvalidOperationException("Not found");
+            }
+
+            oldProduct.ProductPrice = price;
+            this.Ctx.SaveChanges();
+        }
     }
 }
