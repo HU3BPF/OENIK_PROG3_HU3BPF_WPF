c298fc9f7af36e57d8891a14168df52199c8d892
DobiaszMartin <dobiaszmartin@gmail.com>
2021-03-14 16:11:19 +0100

    Manufacturer class added

--- OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Repository/IRepositoryManufacturer.cs
+++ OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Repository/IRepositoryManufacturer.cs
@@ -4,6 +4,7 @@
 
 namespace MobileWebshop.Repository
 {
+    using Microsoft.EntityFrameworkCore;
     using MobileWebshop.Data.Models;
 
     /// <summary>
@@ -43,5 +44,73 @@ namespace MobileWebshop.Repository
         /// <param name="id">Manufacturer id.</param>
         /// <param name="workers">New workers number.</param>
         void ManufacturerWorkersCountChanger(int id, int workers);
+    }
+
+    /// <summary>
+    /// Manufacturer Repository.
+    /// </summary>
+    public class RepositoryManufacturer : Repository<Manufacturer>, IRepositoryManufacturer
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RepositoryManufacturer"/> class.
+        /// </summary>
+        /// <param name="ctx">Dbcontext.</param>
+        public RepositoryManufacturer(DbContext ctx)
+            : base(ctx)
+        {
+        }
+
+        /// <summary>
+        /// One Entity reader.
+        /// </summary>
+        /// <param name="id">Entity id.</param>
+        /// <returns>Entity value.</returns>
+        public override Manufacturer GetOne(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Manufacturer Id Changer.
+        /// </summary>
+        public void ManufacturerIdChanger()
+        {
+        }
+
+        /// <summary>
+        /// Manufacturer CEO update.
+        /// </summary>
+        /// <param name="id">Manufacturer id.</param>
+        /// <param name="ceo">New CEO.</param>
+        public void ManufacturerCEOChanger(int id, string ceo)
+        {
+        }
+
+        /// <summary>
+        /// Manufacturer uptare.
+        /// </summary>
+        /// <param name="id">Manufacturer id.</param>
+        /// <param name="manufacturer">New manufacturer.</param>
+        public void ManufacturerUpdate(int id, Manufacturer manufacturer)
+        {
+        }
+
+        /// <summary>
+        /// Manufacturer reliability.
+        /// </summary>
+        /// <param name="id">Manufacturer id.</param>
+        /// <param name="reliability">New manufacturer reliability.</param>
+        public void ManufacturerReliabilityChanger(int id, int reliability)
+        {
+        }
+
+        /// <summary>
+        /// Workers number changer.
+        /// </summary>
+        /// <param name="id">Manufacturer id.</param>
+        /// <param name="workers">New workers number.</param>
+        public void ManufacturerWorkersCountChanger(int id, int workers)
+        {
+        }
     }
 }
