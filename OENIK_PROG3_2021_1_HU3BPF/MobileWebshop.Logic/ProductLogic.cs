f0ebd65cf9a1ab7d64f6b77aa43ec44447fcf112
DobiaszMartin <dobiaszmartin@gmail.com>
2021-03-14 16:50:06 +0100

    Product logic implemented

---
+++ OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Logic/ProductLogic.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="ProductLogic.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+namespace MobileWebshop.Logic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Product logic.
+    /// </summary>
+    internal class ProductLogic
+    {
+        /// <summary>
+        /// Comparer to 2 Product object.
+        /// </summary>
+        /// <param name="obj">another Product object.</param>
+        /// <returns>Bigger Product price.</returns>
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        /// <summary>
+        /// Generate a Product hash Code.
+        /// </summary>
+        /// <returns>Product hash Code.</returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        /// <summary>
+        /// Ganerate Pruduct object string.
+        /// </summary>
+        /// <returns>String.</returns>
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
