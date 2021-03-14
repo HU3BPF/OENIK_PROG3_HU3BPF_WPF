cb211a4201eb476060b6f279605d51fc8578e9bb
DobiaszMartin <dobiaszmartin@gmail.com>
2021-03-14 19:21:16 +0100

    Database Menu added

--- OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Program/Program.cs
+++ OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Program/Program.cs
@@ -5,6 +5,10 @@
 namespace MobileWebshop.Program
 {
     using System;
+    using ConsoleTools;
+    using MobileWebshop.Data.Models;
+    using MobileWebshop.Logic;
+    using MobileWebshop.Repository;
 
     /// <summary>
     /// The main Program.
@@ -14,7 +18,24 @@ namespace MobileWebshop.Program
         private static void Main(string[] args)
         {
             // Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\MobileWebshopDb.mdf;Integrated Security=True;MultipleActiveResultSets=True
+            MobileDbContext ctx = new MobileDbContext();
+            RepositoryProduct repo = new RepositoryProduct(ctx);
+            ProductLogic logic = new ProductLogic(repo);
+
+            var menu = new ConsoleMenu().Add("Using logic", () => AveragesUsingLogic(logic))
+                .Add("Close", ConsoleMenu.Close);
+            menu.Show();
             Console.WriteLine(args);
+        }
+
+        private static void AveragesUsingLogic(ProductLogic logic)
+        {
+            foreach (var item in logic.GetAllProducts())
+            {
+                Console.WriteLine(item.ProductName);
+            }
+
+            Console.ReadLine();
         }
     }
 }
