d3d4737c5b0ca08cec4da4f7429498959514d58c
DobiaszMartin <dobiaszmartin@gmail.com>
2021-03-16 16:59:16 +0100

    unnecessary using deleted

--- OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Logic/ProductLogic.cs
+++ OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Logic/ProductLogic.cs
@@ -3,11 +3,8 @@
 // </copyright>
 namespace MobileWebshop.Logic
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using MobileWebshop.Data.Models;
     using MobileWebshop.Repository;
 
