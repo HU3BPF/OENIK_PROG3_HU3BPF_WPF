6d5cf6febd564bb19e5fddaefef512698ab45975
DobiaszMartin <dobiaszmartin@gmail.com>
2021-03-16 16:52:10 +0100

    SystemType enum added

---
+++ OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Data/Models/SystemType.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="System.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+namespace MobileWebshop.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    /// <summary>
+    /// Mobile System Type categories.
+    /// </summary>
+    public enum SystemType
+    {
+        /// <summary>
+        /// System Type categori, this is the Android categori.
+        /// </summary>
+        Andorid,
+        /// <summary>
+        /// System Type categori, this is the Android categori.
+        /// </summary>
+        Iso,
+    }
+}
