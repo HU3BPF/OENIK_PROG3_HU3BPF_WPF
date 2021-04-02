f743a3bda1dfb802799e93b50b47113c5b4352ef
DobiaszMartin <dobiaszmartin@gmail.com>
2021-04-02 16:02:32 +0200

    NUllable properties setted.

--- OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Data/Models/Product.cs
+++ OENIK_PROG3_2021_1_HU3BPF/MobileWebshop.Data/Models/Product.cs
@@ -18,45 +18,64 @@ namespace MobileWebshop.Data.Models
         /// </summary>
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+#nullable disable
         public int ProductdId { get; set; }
 
         /// <summary>
         /// Gets or sets Brand id.
         /// </summary>
         [ForeignKey(nameof(Brand))]
-        public int BrandrId { get; set; }
+#nullable enable
+        public int? BrandrId { get; set; }
 
         /// <summary>
         /// Gets or Sets not mapped Brand object.
         /// </summary>
         [NotMapped]
-        public virtual Brand Brand { get; set; }
+#nullable enable
+        public virtual Brand? Brand { get; set; }
 
         /// <summary>
         /// Gets or Sets Product Name.
         /// </summary>
         [MaxLength(100)]
+#nullable disable
         public string ProductName { get; set; }
 
         /// <summary>
         /// Gets or Sets Product price.
         /// </summary>
+#nullable disable
         public int ProductPrice { get; set; }
 
         /// <summary>
         /// Gets or Sets Product Colour.
         /// </summary>
+#nullable disable
         public string ProductColour { get; set; }
 
         /// <summary>
         /// Gets or Sets Product Categories.
         /// </summary>
-        public Category ProductCategori { get; set; }
+#nullable disable
+        public Category ProductCategory { get; set; }
 
         /// <summary>
         /// Gets or Sets Users rating.
         /// </summary>
         [Range(0, 10)]
+#nullable disable
         public int UsresRating { get; set; }
+
+        /// <summary>
+        /// Gets Product string.
+        /// </summary>
+        /// <returns>Product all entities in a string.</returns>
+        public override string ToString()
+        {
+            return $" Product ID: {this.ProductdId}\n Product Name: {this.ProductName}" +
+                $"\n Product Price: {this.ProductPrice}\n Product Colour: {this.ProductColour}" +
+                $"\n Product Category: {this.ProductCategory}\n Product User Rating: {this.UsresRating}\n";
+        }
     }
 }
