207a39f52f8dc8cc2f5abd2ff90bdc36d1d8f13d
DobiaszMartin <dobiaszmartin@gmail.com>
2021-04-06 21:19:29 +0200

    3 non CRUD  tests and 5 CRUD tests Created

--- OENIK_PROG3_2021_1_HU3BPF/Shops.Logic.Test/GoodsManagementLogicTest.cs
+++ OENIK_PROG3_2021_1_HU3BPF/Shops.Logic.Test/GoodsManagementLogicTest.cs
@@ -20,7 +20,8 @@ namespace Shops.Logic.Test
     {
         private Mock<IRepositoryBrand> mockedBrandRepo;
         private Mock<IRepositoryProduct> mockedProductRepo;
-        private List<BrandAverageProductRating> expectedAveragesRating;
+        private List<BrandAverageProductRating> expectedAverageProductRating;
+        private List<BrandAveragerProductPrice> expectedAverageProductPrice;
         private List<Product> expectedProducts;
 
         /// <summary>
@@ -47,41 +48,90 @@ namespace Shops.Logic.Test
         /// Brand Insert MEthod Test.
         /// </summary>
         [Test]
-        public void TestAddBrand()
+        public void TestBrandInserter()
         {
+            // Arrange
             Mock<IRepositoryBrand> mockedBrandRepo = new Mock<IRepositoryBrand>(MockBehavior.Loose);
             Mock<IRepositoryProduct> mockedProductRepo = new Mock<IRepositoryProduct>(MockBehavior.Loose);
             mockedBrandRepo.Setup(repo => repo.Insert(It.IsAny<Brand>()));
             GoodsManagementLogic logic = new GoodsManagementLogic(mockedBrandRepo.Object, mockedProductRepo.Object);
             Brand brand1 = new Brand { BrandName = "test1", BrandNumberOfProducts = 2, BrandAnnualProfit = 500, BrandQuality = 6, NumberOfUsers = 200 };
+
+            // ACT
             logic.BrandInsert(brand1);
+
+            // Assert
             mockedBrandRepo.Verify(repo => repo.Insert(brand1), Times.Once);
+        }
+
+        /// <summary>
+        /// Product Insert Method Test.
+        /// </summary>
+        [Test]
+        public void TestProductInserter()
+        {
+            // Arrange
+            Mock<IRepositoryBrand> mockedBrandRepo = new Mock<IRepositoryBrand>(MockBehavior.Loose);
+            Mock<IRepositoryProduct> mockedProductRepo = new Mock<IRepositoryProduct>(MockBehavior.Loose);
+            mockedProductRepo.Setup(repo => repo.Insert(It.IsAny<Product>()));
+            GoodsManagementLogic logic = new GoodsManagementLogic(mockedBrandRepo.Object, mockedProductRepo.Object);
+            Product product1 = new Product() { ProductName = "Test1", ProductColour = "Pink", ProductPrice = 100, StockNumber = 25, UsresRating = 10, BrandrId = 1, ProductdId = 1 };
+            Product product2 = new Product() { ProductName = "Test2", ProductColour = "Yellow", ProductPrice = 20, StockNumber = 10, UsresRating = 2, BrandrId = 1, ProductdId = 2 };
+            Product product3 = new Product() { ProductName = "Test3", ProductColour = "Blue", ProductPrice = 25, StockNumber = 11, UsresRating = 4, BrandrId = 2, ProductdId = 3 };
+
+            // ACT
+            logic.ProductInsert(product1);
+            logic.ProductInsert(product2);
+            logic.ProductInsert(product3);
+
+            // Assert
+            mockedProductRepo.Verify(repo => repo.Insert(product1), Times.Once);
+            mockedProductRepo.Verify(repo => repo.Insert(product2), Times.Once);
+            mockedProductRepo.Verify(repo => repo.Insert(product3), Times.Once);
         }
 
         /// <summary>
         /// Test Averages RAting Non CRUD METHOD.
         /// </summary>
         [Test]
-        public void TestGetAverages()
+        public void TestGetAverageProductRating()
         {
             // Arrange
-            var logic = this.CreateLogicWithMockToTestGetAveragesRating();
+            var logic = this.CreateLogicWithMockToTestGetAverageProductRating();
 
             // Act
             var actualAveragesRating = logic.GetBrandAveragesRating();
 
             // Assert
-            Assert.That(actualAveragesRating, Is.EquivalentTo(this.expectedAveragesRating));
+            Assert.That(actualAveragesRating, Is.EquivalentTo(this.expectedAverageProductRating));
             this.mockedBrandRepo.Verify(repo => repo.GetALL(), Times.Once);
             this.mockedProductRepo.Verify(repo => repo.GetALL(), Times.Once);
         }
 
-        private GoodsManagementLogic CreateLogicWithMockToTestGetAveragesRating()
+        /// <summary>
+        /// Test Averages Price Non CRUD METHOD.
+        /// </summary>
+        [Test]
+        public void TestGetAverageProductPrice()
+        {
+            // Arrange
+            var logic = this.CreateLogicWithMockToTestGetAverageProductPrice();
+
+            // Act
+            var actualAveragesPrice = logic.GetBrandAveragesPrice();
+
+            // Assert
+            Assert.That(actualAveragesPrice, Is.EquivalentTo(this.expectedAverageProductPrice));
+            this.mockedBrandRepo.Verify(repo => repo.GetALL(), Times.Once);
+            this.mockedProductRepo.Verify(repo => repo.GetALL(), Times.Once);
+        }
+
+        private GoodsManagementLogic CreateLogicWithMockToTestGetAverageProductRating()
         {
             this.mockedProductRepo = new Mock<IRepositoryProduct>();
             this.mockedBrandRepo = new Mock<IRepositoryBrand>();
-            Brand test1 = new Brand() { BrandName = "test1", BrandNumberOfProducts = 2, BrandAnnualProfit = 500, BrandQuality = 6, NumberOfUsers = 200, BrandId = 1 };
-            Brand test2 = new Brand() { BrandName = "test2", BrandNumberOfProducts = 2, BrandAnnualProfit = 500, BrandQuality = 6, NumberOfUsers = 200, BrandId = 2 };
+            Brand test1 = new Brand() { BrandName = "test1", BrandNumberOfProducts = 4, BrandAnnualProfit = 500, BrandQuality = 6, NumberOfUsers = 200, BrandId = 1 };
+            Brand test2 = new Brand() { BrandName = "test2", BrandNumberOfProducts = 3, BrandAnnualProfit = 500, BrandQuality = 6, NumberOfUsers = 200, BrandId = 2 };
             List<Brand> brands = new List<Brand>() { test1, test2 };
             List<Product> products = new List<Product>()
             {
@@ -93,10 +143,37 @@ namespace Shops.Logic.Test
                 new Product() { ProductName = "Test6", ProductColour = "Green", ProductPrice = 44, StockNumber = 33, UsresRating = 8, BrandrId = test2.BrandId, ProductdId = 6 },
                 new Product() { ProductName = "Test7", ProductColour = "Pink", ProductPrice = 100, StockNumber = 25, UsresRating = 9, BrandrId = test2.BrandId, ProductdId = 7 },
             };
-            this.expectedAveragesRating = new List<BrandAverageProductRating>()
+            this.expectedAverageProductRating = new List<BrandAverageProductRating>()
             {
                 new BrandAverageProductRating() { BrandName = "test2", AverageRating = 8 },
-                new BrandAverageProductRating() { BrandName = "test1", AverageRating = 5 },
+                new BrandAverageProductRating() { BrandName = "test1", AverageRating = 5.25 },
+            };
+            this.mockedBrandRepo.Setup(repo => repo.GetALL()).Returns(brands.AsQueryable());
+            this.mockedProductRepo.Setup(repo => repo.GetALL()).Returns(products.AsQueryable());
+            return new GoodsManagementLogic(this.mockedBrandRepo.Object, this.mockedProductRepo.Object);
+        }
+
+        private GoodsManagementLogic CreateLogicWithMockToTestGetAverageProductPrice()
+        {
+            this.mockedProductRepo = new Mock<IRepositoryProduct>();
+            this.mockedBrandRepo = new Mock<IRepositoryBrand>();
+            Brand test1 = new Brand() { BrandName = "test1", BrandNumberOfProducts = 2, BrandAnnualProfit = 500, BrandQuality = 6, NumberOfUsers = 200, BrandId = 1 };
+            Brand test2 = new Brand() { BrandName = "test2", BrandNumberOfProducts = 2, BrandAnnualProfit = 500, BrandQuality = 6, NumberOfUsers = 200, BrandId = 2 };
+            List<Brand> brands = new List<Brand>() { test1, test2 };
+            List<Product> products = new List<Product>()
+            {
+                new Product() { ProductName = "Test1", ProductColour = "Pink", ProductPrice = 94, StockNumber = 25, UsresRating = 10, BrandrId = test1.BrandId, ProductdId = 1 },
+                new Product() { ProductName = "Test2", ProductColour = "Yellow", ProductPrice = 50, StockNumber = 10, UsresRating = 2, BrandrId = test1.BrandId, ProductdId = 2 },
+                new Product() { ProductName = "Test3", ProductColour = "Blue", ProductPrice = 25, StockNumber = 11, UsresRating = 4, BrandrId = test1.BrandId, ProductdId = 3 },
+                new Product() { ProductName = "Test4", ProductColour = "Pink", ProductPrice = 30, StockNumber = 10, UsresRating = 5, BrandrId = test1.BrandId, ProductdId = 4 },
+                new Product() { ProductName = "Test5", ProductColour = "Black", ProductPrice = 55, StockNumber = 25, UsresRating = 7, BrandrId = test2.BrandId, ProductdId = 5 },
+                new Product() { ProductName = "Test6", ProductColour = "Green", ProductPrice = 44, StockNumber = 33, UsresRating = 8, BrandrId = test2.BrandId, ProductdId = 6 },
+                new Product() { ProductName = "Test7", ProductColour = "Pink", ProductPrice = 78, StockNumber = 25, UsresRating = 9, BrandrId = test2.BrandId, ProductdId = 7 },
+            };
+            this.expectedAverageProductPrice = new List<BrandAveragerProductPrice>()
+            {
+                new BrandAveragerProductPrice() { BrandName = "test2", AveragePrice = 59 },
+                new BrandAveragerProductPrice() { BrandName = "test1", AveragePrice = 49.75 },
             };
             this.mockedBrandRepo.Setup(repo => repo.GetALL()).Returns(brands.AsQueryable());
             this.mockedProductRepo.Setup(repo => repo.GetALL()).Returns(products.AsQueryable());
--- OENIK_PROG3_2021_1_HU3BPF/Shops.Logic.Test/ShopManagementLogicTest.cs
+++ OENIK_PROG3_2021_1_HU3BPF/Shops.Logic.Test/ShopManagementLogicTest.cs
@@ -4,7 +4,13 @@
 
 namespace Shops.Logic.Test
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using Moq;
     using NUnit.Framework;
+    using Shops.Data.Models;
+    using Shops.Logic.NonCrudClasses;
+    using Shops.Repository;
 
     /// <summary>
     /// Shop management Logic Test.
@@ -12,5 +18,107 @@ namespace Shops.Logic.Test
     [TestFixture]
     public class ShopManagementLogicTest
     {
+        private Mock<IRepositoryShop> mockedShopRepo;
+        private Mock<IRepositoryProduct> mockedProductRepo;
+        private List<ShopNumberOfProduct> expectedShopNumberOfProduct;
+
+        /// <summary>
+        /// Test Get Shop Number Of Product.
+        /// </summary>
+        [Test]
+        public void TestGetShopNumberOfProduct()
+        {
+            // Arrange
+            var logic = this.CreateLogicWithMockToTestGetShopNumberOfProduct();
+
+            // ACT
+            var actualNumberOfProducts = logic.GetNumberOfProducts();
+
+            // Assert
+            Assert.That(actualNumberOfProducts, Is.EquivalentTo(this.expectedShopNumberOfProduct));
+            this.mockedProductRepo.Verify(pRepo => pRepo.GetALL(), Times.Once);
+            this.mockedShopRepo.Verify(sRepo => sRepo.GetALL(), Times.Once);
+        }
+
+        /// <summary>
+        /// Test Shop inserter Method.
+        /// </summary>
+        [Test]
+        public void TestShopInsert()
+        {
+            // Arrange
+            Mock<IRepositoryShop> mockedShopRepo = new Mock<IRepositoryShop>(MockBehavior.Loose);
+            Mock<IRepositoryProduct> mockedProductRepo = new Mock<IRepositoryProduct>(MockBehavior.Loose);
+            mockedShopRepo.Setup(repo => repo.Insert(It.IsAny<Shop>()));
+            ShopManagementLogic logic = new ShopManagementLogic(mockedShopRepo.Object, mockedProductRepo.Object);
+            Shop test1 = new Shop() { ShopName = "test1", ShopLocation = "Alaszka", ShopAnnualProfit = 1000, ShopNumberOfWorker = 60, ShopReliability = 10, ShopLeader = "Hulk", ShopId = 1 };
+            Shop test2 = new Shop() { ShopName = "test2", ShopLocation = "New York", ShopAnnualProfit = 200, ShopNumberOfWorker = 2, ShopReliability = 10, ShopLeader = "Thor", ShopId = 2 };
+            Shop test3 = new Shop() { ShopName = "test3", ShopLocation = "Atlanta", ShopAnnualProfit = 300, ShopNumberOfWorker = 4, ShopReliability = 7, ShopLeader = "Hulk", ShopId = 3 };
+
+            // ACT
+            logic.ShopInsert(test1);
+            logic.ShopInsert(test2);
+            logic.ShopInsert(test3);
+
+            // Assert
+            mockedShopRepo.Verify(repo => repo.Insert(test1), Times.Once);
+            mockedShopRepo.Verify(repo => repo.Insert(test2), Times.Once);
+            mockedShopRepo.Verify(repo => repo.Insert(test3), Times.Once);
+        }
+
+        /// <summary>
+        /// Test Shop Remover Method.
+        /// </summary>
+        [Test]
+        public void TestShopRemover()
+        {
+            // Arrange
+            Mock<IRepositoryShop> mockedShopRepo = new Mock<IRepositoryShop>(MockBehavior.Loose);
+            Mock<IRepositoryProduct> mockedProductRepo = new Mock<IRepositoryProduct>(MockBehavior.Loose);
+            mockedShopRepo.Setup(repo => repo.Remove(It.IsAny<Shop>()));
+            ShopManagementLogic logic = new ShopManagementLogic(mockedShopRepo.Object, mockedProductRepo.Object);
+            Shop test1 = new Shop() { ShopName = "test1", ShopLocation = "Alaszka", ShopAnnualProfit = 1000, ShopNumberOfWorker = 60, ShopReliability = 10, ShopLeader = "Hulk", ShopId = 1 };
+            Shop test2 = new Shop() { ShopName = "test2", ShopLocation = "New York", ShopAnnualProfit = 200, ShopNumberOfWorker = 2, ShopReliability = 10, ShopLeader = "Thor", ShopId = 2 };
+            Shop test3 = new Shop() { ShopName = "test3", ShopLocation = "Atlanta", ShopAnnualProfit = 300, ShopNumberOfWorker = 4, ShopReliability = 7, ShopLeader = "Hulk", ShopId = 3 };
+
+            // ACT
+            logic.ShopRemove(test1);
+            logic.ShopRemove(test2);
+            logic.ShopRemove(test3);
+
+            // Assert
+            mockedShopRepo.Verify(repo => repo.Remove(test1), Times.Once);
+            mockedShopRepo.Verify(repo => repo.Remove(test2), Times.Once);
+            mockedShopRepo.Verify(repo => repo.Remove(test3), Times.Once);
+        }
+
+        private ShopManagementLogic CreateLogicWithMockToTestGetShopNumberOfProduct()
+        {
+            this.mockedProductRepo = new Mock<IRepositoryProduct>();
+            this.mockedShopRepo = new Mock<IRepositoryShop>();
+            Shop test1 = new Shop() { ShopName = "test1", ShopLocation = "Alaszka", ShopAnnualProfit = 500, ShopNumberOfWorker = 6, ShopReliability = 200, ShopLeader = "Hulk", ShopId = 1 };
+            Shop test2 = new Shop() { ShopName = "test2", ShopLocation = "New York", ShopAnnualProfit = 500, ShopNumberOfWorker = 6, ShopReliability = 200, ShopLeader = "Thor", ShopId = 2 };
+            Brand test3 = new Brand() { BrandName = "test1", BrandNumberOfProducts = 2, BrandAnnualProfit = 500, BrandQuality = 6, NumberOfUsers = 200, BrandId = 1, ShopID = 1 };
+            Brand test4 = new Brand() { BrandName = "test2", BrandNumberOfProducts = 2, BrandAnnualProfit = 500, BrandQuality = 6, NumberOfUsers = 200, BrandId = 2, ShopID = 2 };
+            List<Shop> shops = new List<Shop>() { test1, test2 };
+            List<Product> products = new List<Product>()
+            {
+                new Product() { ProductName = "Test1", ProductColour = "Pink", ProductPrice = 100, StockNumber = 25, UsresRating = 10, BrandrId = test3.BrandId, ProductdId = 1, Brand = test3 },
+                new Product() { ProductName = "Test2", ProductColour = "Yellow", ProductPrice = 20, StockNumber = 10, UsresRating = 2, BrandrId = test3.BrandId, ProductdId = 2, Brand = test3 },
+                new Product() { ProductName = "Test3", ProductColour = "Blue", ProductPrice = 25, StockNumber = 11, UsresRating = 4, BrandrId = test3.BrandId, ProductdId = 3, Brand = test3 },
+                new Product() { ProductName = "Test4", ProductColour = "Pink", ProductPrice = 30, StockNumber = 10, UsresRating = 5, BrandrId = test3.BrandId, ProductdId = 4, Brand = test3 },
+                new Product() { ProductName = "Test5", ProductColour = "Black", ProductPrice = 55, StockNumber = 25, UsresRating = 7, BrandrId = test4.BrandId, ProductdId = 5, Brand = test4 },
+                new Product() { ProductName = "Test6", ProductColour = "Green", ProductPrice = 44, StockNumber = 33, UsresRating = 8, BrandrId = test4.BrandId, ProductdId = 6, Brand = test4 },
+                new Product() { ProductName = "Test7", ProductColour = "Pink", ProductPrice = 100, StockNumber = 25, UsresRating = 9, BrandrId = test4.BrandId, ProductdId = 7, Brand = test4 },
+            };
+            this.expectedShopNumberOfProduct = new List<ShopNumberOfProduct>()
+            {
+                new ShopNumberOfProduct() { ShopName = "test1", NumberOfProduct = 4 },
+                new ShopNumberOfProduct() { ShopName = "test2", NumberOfProduct = 3 },
+            };
+            this.mockedShopRepo.Setup(repo => repo.GetALL()).Returns(shops.AsQueryable());
+            this.mockedProductRepo.Setup(repo => repo.GetALL()).Returns(products.AsQueryable());
+            return new ShopManagementLogic(this.mockedShopRepo.Object, this.mockedProductRepo.Object);
+        }
     }
 }
