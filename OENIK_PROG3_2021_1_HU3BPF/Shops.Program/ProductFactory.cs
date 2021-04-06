57335eb017ee64b3d2fa4870dc5f5c2dcf44d08a
DobiaszMartin <dobiaszmartin@gmail.com>
2021-04-06 20:07:57 +0200

    Get by brands method

--- OENIK_PROG3_2021_1_HU3BPF/Shops.Program/ProductFactory.cs
+++ OENIK_PROG3_2021_1_HU3BPF/Shops.Program/ProductFactory.cs
@@ -30,6 +30,44 @@ namespace Shops.Program
         }
 
         /// <summary>
+        /// Get All products.
+        /// </summary>
+        /// <param name="logic">product logic.</param>
+        internal static void GetAllProductByBrand(GoodsManagementLogic logic)
+        {
+            NumberFormatInfo provider = new NumberFormatInfo();
+            Console.Clear();
+            Console.WriteLine("Write the Brand ID".ToString());
+            try
+            {
+                int id = int.Parse(Console.ReadLine(), provider); // Rossz id esetén rossz!!!.
+                Console.Clear();
+                Console.WriteLine("Brand Name: " + logic.BrandGetOne(id).BrandName?.ToString() + "\n");
+                Console.WriteLine("Products: ".ToString());
+                foreach (var item in logic?.GetProductByBrand(id))
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+            catch (FormatException exception)
+            {
+                Console.WriteLine(exception.Message);
+                Console.WriteLine("Press any key to contnue.".ToString());
+                Console.ReadKey();
+                GetONeProduct(logic);
+            }
+            catch (ArgumentNullException exception)
+            {
+                Console.WriteLine(exception.Message);
+                Console.WriteLine("Press any key to contnue.".ToString());
+                Console.ReadKey();
+                GetONeProduct(logic);
+            }
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
         /// Gets One product.
         /// </summary>
         /// <param name="logic">product logic.</param>
