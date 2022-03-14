4e9f6c73478f2a673824871bd86e12e9ed1cd95b
DobiaszMartin <1998dobiasz@gmail.com>
2022-03-14 16:54:06 +0100

    Added Swagger.

--- Shops.Endpoint/Startup.cs
+++ Shops.Endpoint/Startup.cs
@@ -6,6 +6,10 @@ using Microsoft.EntityFrameworkCore;
 using Shops.Repository;
 using Shops.Logic;
 using Shops.Data.Models;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.OpenApi.Models;
+using Shops.Endpoint.Services;
 
 namespace Shops.Endpoint
 {
@@ -23,22 +27,45 @@ namespace Shops.Endpoint
             services.AddTransient<IRepositoryBrand, BrandRepository>();
             services.AddTransient<IRepositoryShop, RepositoryShop>();
             services.AddTransient<DbContext, ShopsDbContext>();
+
+            services.AddSignalR();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Shops.Endpoint", Version = "v1" });
+            });
         }
 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+          public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shops.Endpoint v1"));
             }
 
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features
+                    .Get<IExceptionHandlerPathFeature>()
+                    .Error;
+                var response = new { Msg = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
+
+
             app.UseRouting();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<SignalRHub>("/hub");
             });
         }
     }
--- Shops.Program/RestService.cs
+++ Shops.Program/RestService.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Shops.Client
 {
@@ -8,9 +12,28 @@ namespace Shops.Client
     {
         HttpClient client;
 
-        public RestService(string baseurl)
+        public RestService(string baseurl, string pingableEndpoint = "swagger")
         {
+            bool isOk = false;
+            do
+            {
+                isOk = Ping(baseurl + pingableEndpoint);
+            } while (isOk == false);
             Init(baseurl);
+        }
+
+        private bool Ping(string url)
+        {
+            try
+            {
+                WebClient wc = new WebClient();
+                wc.DownloadData(url);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         private void Init(string baseurl)
@@ -40,6 +63,11 @@ namespace Shops.Client
             {
                 items = response.Content.ReadAsAsync<List<T>>().GetAwaiter().GetResult();
             }
+            else
+            {
+                var error = response.Content.ReadAsAsync<RestExceptionInfo>().GetAwaiter().GetResult();
+                throw new ArgumentException(error.Msg);
+            }
             return items;
         }
 
@@ -50,6 +78,11 @@ namespace Shops.Client
             if (response.IsSuccessStatusCode)
             {
                 item = response.Content.ReadAsAsync<T>().GetAwaiter().GetResult();
+            }
+            else
+            {
+                var error = response.Content.ReadAsAsync<RestExceptionInfo>().GetAwaiter().GetResult();
+                throw new ArgumentException(error.Msg);
             }
             return item;
         }
@@ -62,6 +95,11 @@ namespace Shops.Client
             {
                 item = response.Content.ReadAsAsync<T>().GetAwaiter().GetResult();
             }
+            else
+            {
+                var error = response.Content.ReadAsAsync<RestExceptionInfo>().GetAwaiter().GetResult();
+                throw new ArgumentException(error.Msg);
+            }
             return item;
         }
 
@@ -70,6 +108,11 @@ namespace Shops.Client
             HttpResponseMessage response =
                 client.PostAsJsonAsync(endpoint, item).GetAwaiter().GetResult();
 
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = response.Content.ReadAsAsync<RestExceptionInfo>().GetAwaiter().GetResult();
+                throw new ArgumentException(error.Msg);
+            }
             response.EnsureSuccessStatusCode();
         }
 
@@ -77,6 +120,12 @@ namespace Shops.Client
         {
             HttpResponseMessage response =
                 client.DeleteAsync(endpoint + "/" + id.ToString()).GetAwaiter().GetResult();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = response.Content.ReadAsAsync<RestExceptionInfo>().GetAwaiter().GetResult();
+                throw new ArgumentException(error.Msg);
+            }
 
             response.EnsureSuccessStatusCode();
         }
@@ -86,9 +135,22 @@ namespace Shops.Client
             HttpResponseMessage response =
                 client.PutAsJsonAsync(endpoint, item).GetAwaiter().GetResult();
 
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = response.Content.ReadAsAsync<RestExceptionInfo>().GetAwaiter().GetResult();
+                throw new ArgumentException(error.Msg);
+            }
 
             response.EnsureSuccessStatusCode();
         }
 
+    }
+    public class RestExceptionInfo
+    {
+        public RestExceptionInfo()
+        {
+
+        }
+        public string Msg { get; set; }
     }
 }
